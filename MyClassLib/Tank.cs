dfda21824af558267a4289f43140ef8ac766c94c
presidentofkazakhstan <alibekbekhan@gmail.com>
2019-03-21 22:43:10 +0600

    homework

---
+++ MyClassLib/Tank.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLib
+{
+    public class Tank
+    {
+        public string NameOfTank { get; set; }
+        public int LevelTankAmmunition { get; set; }
+        public int LevelTankArmor { get; set; }
+        public int LevelTankManeuverability { get; set; }
+        public Tank() { }
+        public Tank(string nameOfTank, int levelTankArmor, int levelTankAmmunition, int levelTankManeuverability)
+        {
+            Random random = new Random();
+            NameOfTank = nameOfTank;
+            LevelTankAmmunition = random.Next(0, 100);
+            LevelTankArmor = random.Next(0, 100);
+            LevelTankManeuverability = random.Next(0, 100);
+        }
+
+        public static bool operator ^(Tank obj1, Tank obj2)
+        {
+            Tank obj = new Tank();
+            if (obj1.LevelTankArmor > obj2.LevelTankArmor && obj1.LevelTankAmmunition > obj2.LevelTankAmmunition && obj1.LevelTankManeuverability > obj2.LevelTankManeuverability || obj1.LevelTankArmor < obj2.LevelTankArmor && obj1.LevelTankAmmunition > obj1.LevelTankAmmunition && obj1.LevelTankManeuverability > obj2.LevelTankAmmunition || obj1.LevelTankArmor > obj2.LevelTankArmor && obj1.LevelTankAmmunition < obj2.LevelTankAmmunition && obj1.LevelTankManeuverability > obj2.LevelTankManeuverability || obj1.LevelTankArmor > obj2.LevelTankArmor && obj1.LevelTankAmmunition > obj2.LevelTankAmmunition && obj1.LevelTankManeuverability < obj2.LevelTankManeuverability)
+                return true;
+            return false;
+        }
+        public static bool operator *(Tank obj1, Tank obj2)
+        {
+            Tank obj = new Tank();
+            if (obj1.LevelTankArmor == obj2.LevelTankArmor && obj1.LevelTankAmmunition == obj2.LevelTankAmmunition && obj1.LevelTankManeuverability == obj2.LevelTankManeuverability)
+                return true;
+            return false;
+        }
+    }
+}
---
+++ module_07/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClassLib;
+
+
+namespace module_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tank tank = new Tank("T-34", 0, 0, 0);
+            Tank secondTank = new Tank("Panthera", 4, 5, 6);
+            Tank thirdTank = new Tank("RPG", 1, 2, 3);
+            Tank fourthTank = new Tank("Kuz`ma", 7, 8, 9);
+            Tank fifthTank = new Tank("Alfred", 10, 11, 12);
+            Console.WriteLine("Уровень защиты первого танка: " + tank.LevelTankArmor);
+            Console.WriteLine("Уровень боезапаса первого танка: " + tank.LevelTankAmmunition);
+            Console.WriteLine("Уровень манёврености первого танка: " + tank.LevelTankManeuverability);
+
+
+            Console.WriteLine("Уровень защиты второго танка: " + secondTank.LevelTankArmor);
+            Console.WriteLine("Уровень боезапаса второго танка: " + secondTank.LevelTankAmmunition);
+            Console.WriteLine("Уровень манёврености второго танка: " + secondTank.LevelTankManeuverability);
+            if (tank ^ secondTank)
+            {
+                Console.WriteLine("Первый танк выиграл");
+            }
+            else if (tank * secondTank)
+            {
+                Console.WriteLine("Ничья");
+            }
+            else
+            {
+                Console.WriteLine("Второй танк выиграл");
+            }
+
+            Console.Read();
+        }
+    }
+}
